250a5c9dfb7d9ecb638b9ee6417118d6bbac383e
pyhtohbs <84662877+pyhtohbs@users.noreply.github.com>
2021-05-27 19:40:05 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,25 +9,25 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @W2HSupport.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
+        f"""**Hey, I'm  {bn}, an open-source bot that lets you play music in your Telegram groups.For Support Join our group @python_pirates.\n\n The Assistant must be in your group to play music in the voice chat of your group.\n\n /help to know my commands**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Support⚡️", url="https://t.me/W2HSupport"
+                        "Support⚡️", url="https://t.me/python_pirates"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Creater⚡️", url="https://t.me/David99q"
+                        "Creater⚡️", url="https://t.me/your_handler"
                     ),
                     InlineKeyboardButton(
-                        "Ravan102030⚡️", url="https://t.me/Ravan102030"
+                        "hayathome⚡️", url="https://t.me/pyar_lafzonmein_kahan"
                     )
                 ],
                 [
                     InlineKeyboardButton(
-                        "Add To Your Group⚡️", url="https://t.me/W2H_Music_1_Bot?startgroup=true"
+                        "Add To Your Group⚡️", url="https://t.me/hayat_vc_bot?startgroup=true"
                     )
                 ]
             ]
